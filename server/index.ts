37bd95537060cf35cc505e7834e98678600e535b
Davidayo <davidayo@Davidayos-MacBook.local>
2025-01-14 02:48:15 -0600

    add scripts

--- server/index.ts
+++ server/index.ts
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
   }
 
   // ALWAYS serve the app on port 3000
-  const PORT = 3000;
+  const PORT = 8000;
   server.listen(PORT, "0.0.0.0", () => {
     log(`Serving on port ${PORT}`);
   });
