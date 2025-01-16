176df01a7fbaf43e3ac9fabc6680e242385a4bfb
Davidayo <davidayo@Davidayos-MacBook.local>
2025-01-16 02:25:06 -0600

    add scripts

--- server/index.ts
+++ server/index.ts
@@ -45,3 +45,31 @@ app.use((req, res, next) => {
 
   next();
 });
+
+(async () => {
+  const server = registerRoutes(app);
+
+  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    const status = err.status || err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    res.status(status).json({ message });
+    throw err;
+  });
+
+  // importantly only setup vite in development and after
+  // setting up all the other routes so the catch-all route
+  // doesn't interfere with the other routes
+  if (app.get("env") === "development") {
+    await setupVite(app, server);
+  } else {
+    serveStatic(app);
+  }
+
+  // ALWAYS serve the app on port 3000
+  // this serves both the API and the client
+  const PORT = 3000;
+  server.listen(PORT, "0.0.0.0", () => {
+    log(`serving on port ${PORT}`);
+  });
+})();
