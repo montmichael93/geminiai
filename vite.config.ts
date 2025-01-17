3d9dc6ccf92fa396dff2ac7f252c9f8f979055e7
Davidayo <davidayo@Davidayos-MacBook.local>
2025-01-17 03:11:32 -0600

    make it better

--- vite.config.ts
+++ vite.config.ts
@@ -9,16 +9,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), runtimeErrorOverlay(), themePlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@db": path.resolve(__dirname, "db"),
       "@": path.resolve(__dirname, "client", "src"),
     },
   },
-  root: path.resolve(__dirname, "client"),
+  root: 'client',
   build: {
-    outDir: path.resolve(__dirname, "client/index.html"),
+    outDir: 'dist/public',
     emptyOutDir: true,
 
   },
@@ -27,3 +27,4 @@ export default defineConfig({
     strictPort: true, // Fail if the port is already in use
   },
 });
+
