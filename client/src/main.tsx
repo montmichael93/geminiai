49b325c7e008982341bca7a92a53bc7595eb8549
mnn jhn <michaelmontero9319@gmail.com>
2025-03-10 16:05:23 -0400

    deploy fix attempt

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/toaster";
-import App from './App';
+
+import App from "./App";
 import "./index.css";
+import { Toaster } from "./components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,5 +13,5 @@ createRoot(document.getElementById("root")!).render(
       <App />
       <Toaster />
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 );
